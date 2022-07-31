a8b7f53ed8c307bd22a4f40b2ed83de327456cd4
hayderAbdulsahib <abdulsahib.hayder@gmail.com>
2022-07-31 13:00:28 +0300

    solving Q6

---
+++ Q6/app.js
@@ -0,0 +1,57 @@
+let monthsLabel = [];
+let numbersLabel = [];
+
+const chartData = async () => {
+  try {
+    const res = await axios.get(
+      "https://my-json-server.typicode.com/ahmad-athra/exam1/ChartData"
+    );
+
+    const chartApiData = res.data[0];
+
+    const months = chartApiData.xaxis.map((element) => {
+      return element;
+    });
+
+    const numbers = chartApiData.yaxis.map((element) => {
+      return element;
+    });
+
+    monthsLabel = months;
+    numbersLabel = numbers;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const dummyChartArrow = async () => {
+  await chartData();
+
+  const ctx = document.getElementById("myChart").getContext("2d");
+
+  const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: "line",
+
+    // The data for our dataset
+    data: {
+      labels: monthsLabel,
+      datasets: [
+        {
+          label: "Number",
+          backgroundColor: "pink",
+          borderColor: "rgb(255, 99, 132)",
+          data: numbersLabel,
+        },
+      ],
+    },
+
+    options: {
+      interaction: {
+        mode: "index",
+      },
+    },
+  });
+};
+
+dummyChartArrow();
